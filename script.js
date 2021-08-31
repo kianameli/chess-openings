8dcd598804c401a6c1e0bb7a26dcd821075abe67
Kian Ameli <kianameli@Kians-MBP.attlocal.net>
2021-08-31 16:03:26 -0400

    cleaner functionality, add some css

--- script.js
+++ script.js
@@ -145,8 +145,8 @@ function handleRuyLopezClick() {
     "r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R b KQkq - 3 3";
   const ruyLopezPGN = "1. e4 e5 2. Nf3 Nc6 3. Bb5";
   game.reset();
-  game.load(ruyLopezFEN);
   board.position(ruyLopezFEN);
+  game.load_pgn(ruyLopezPGN);
   updateStatus();
   resetOpeningInfo();
   //document.querySelector("#pgn").innerHTML = ruyLopezPGN;
